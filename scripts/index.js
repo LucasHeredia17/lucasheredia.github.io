2954eab289da69aee5ec342f4ba568b92307d877
Lucas Heredia <heredialucas1@gmail.com>
2024-06-04 15:45:24 -0300

    update: added section 'my proyects'

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,7 @@ function scrollToElement(elementSelector, instance = 0) {
 
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
 
 link1.addEventListener("click", () => {
   scrollToElement(".tituloprincipal1");
@@ -16,6 +17,10 @@ link1.addEventListener("click", () => {
 
 link2.addEventListener("click", () => {
   scrollToElement(".tituloprincipal2");
+});
+
+link3.addEventListener("click", () => {
+  scrollToElement(".tituloprincipal3");
 });
 
 //* Añadir actividad
